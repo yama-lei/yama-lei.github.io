8157994fc8646ce0d76a66467343027df69b5f4e
yama-lei <1908777046@qq.com>
2025-02-10 14:14:20 +0000

    Deploying to gh-pages from @ yama-lei/yama-lei.github.io@40cd5063066ff2d42b19abaae053221574b4d9d2 🚀

---
+++ assets/index.html-CRDKNhpR.js
@@ -0,0 +1,7 @@
+import{_ as a,c as t,a as n,e as i,o as s,r as o}from"./app-BPDNkl4K.js";const r={};function d(l,m){const e=o("myCard");return s(),t("div",null,[n(`-
+<VPCard
+    title="MateXiao's blog"
+    logo="/assets/friendsHead/a.png"
+    desc="这是MateXiao的个人博客"
+    link="https://meta-xiao.github.io/"
+/>`),i(e,{title:"HappyNewYear!",logo:"/assets/friendsHead/WangYuQin.jpg",desc:"Have a nice Spring Festivel and have fun!",link:"/happy/index.html"})])}const p=a(r,[["render",d],["__file","index.html.vue"]]),f=JSON.parse('{"path":"/friendsLink/","title":"","lang":"zh-CN","frontmatter":{"feed":false,"seo":false,"head":[]},"headers":[],"git":{"createdTime":1737788921000,"updatedTime":1738072089000,"contributors":[{"name":"yama-lei","username":"yama-lei","email":"1908777046@qq.com","commits":4,"url":"https://github.com/yama-lei"}]},"readingTime":{"minutes":0.13,"words":38},"filePathRelative":"friendsLink/README.md","localizedDate":"2025年1月25日"}');export{p as comp,f as data};
